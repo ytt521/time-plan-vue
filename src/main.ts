9e4ab9a0d7fe479c23a3a40ec2ad3bf23cec27e4
挚爱小v脸 <3047893225@qq.com>
2024-01-14 19:23:50 +0800

    build: init

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//@ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import 'virtual:svg-icons-register'
+
+import '@/styles/index.scss'
+
+const app = createApp(App)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+
+app.mount('#app')
